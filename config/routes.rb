b2a1779bcd03313a8203a9a9d280ad4a3eb11127
ryoide <ide@spacemarket.co.jp>
2017-04-15 20:34:05 +0900

    fix bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'callback', to: 'webhook#callback'
+  post 'callback' => 'webhook#callback'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
