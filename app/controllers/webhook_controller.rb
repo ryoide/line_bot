40fc6e1da1f2cadcb7bc96f77432bf847f65699e
ryoide <ide@spacemarket.co.jp>
2017-04-15 18:43:22 +0900

    fix web hook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,4 +1,6 @@
 class WebhookController < ApplicationController
+  require 'sinatra'
+  require 'line/bot'
   protect_from_forgery with: :null_session
   
   CHANNEL_SECRET = '28227f54c66cb6e4dc7b1d2efc108bdb'
@@ -9,20 +11,30 @@ class WebhookController < ApplicationController
     unless is_validate_signature
       render :nothing => true, status: 470
     end
-    result = params[:result][0]
-    logger.info({from_line: result})
-    text_message = result['content']['text']
-    from_mid =result['content']['from']
-
-    client = LineClient.new(CHANNEL_SECRET, CHANNEL_ACCESS_TOKEN, OUTBOUND_PROXY)
-    res = client.send([from_mid], text_message)
-
-    if res.status == 200
-      logger.info({success: res})
-    else
-      logger.info({fail: res})
-    end
-    render :nothing => true, status: :ok
+    client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+    body = request.body.read
+    events = client.parse_events_from(body)
+    events.each { |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    }
+    "OK"
   end
   
   private
